26ee3fbbd109ae6bf5893507eae5a0e99011e763
YahyaDalbah <yahya1dalbah@gmail.com>
2025-04-18 22:58:34 +0300

    set up firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+import 'package:wassilni/firebase_options.dart';
 import 'package:wassilni/pages/map.dart';
 
 void main() async {
@@ -9,6 +11,7 @@ void main() async {
 }
 
 Future<void> setup() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await dotenv.load(fileName: ".env");
   MapboxOptions.setAccessToken(dotenv.env["MAPBOX_ACCESS_TOKEN"]!);
 }
