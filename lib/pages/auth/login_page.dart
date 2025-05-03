d627e10a728ac45bad6266938f764b15b439d5c8
YahyaDalbah <yahya1dalbah@gmail.com>
2025-05-03 14:42:20 +0300

    made login direct to home page

--- lib/pages/auth/login_page.dart
+++ lib/pages/auth/login_page.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:wassilni/pages/auth/register_page.dart';
+import 'package:wassilni/pages/home_page.dart';
 import 'package:wassilni/pages/map.dart';
 
 class LoginPage extends StatefulWidget {
@@ -42,7 +43,7 @@ class _LoginPageState extends State<LoginPage> {
       );
       if (mounted) {
         Navigator.of(context).pushReplacement(
-          MaterialPageRoute(builder: (context) => Map()),
+          MaterialPageRoute(builder: (context) => HomePage()),
         );
       }
     } catch (e) {
