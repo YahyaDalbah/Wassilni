6bbf0d662ecff6456d97dd79d292ad502f2e4b66
marselHanani <s12220672@stu.najah.edu>
2025-05-03 12:26:06 +0300

    create authentication pages by marsel

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:wassilni/firebase_options.dart';
+import 'package:wassilni/pages/auth/information_page.dart';
+import 'package:wassilni/pages/auth/login_page.dart';
+import 'package:wassilni/pages/auth/register_page.dart';
 import 'package:wassilni/pages/map.dart';
 import 'package:wassilni/providers/destination_provider.dart';
 import 'package:wassilni/providers/fare_provider.dart';
@@ -33,6 +36,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Flutter Demo', home: const Map());
+    return MaterialApp(title: 'Flutter Demo', home: const Registerpage());
   }
 }
