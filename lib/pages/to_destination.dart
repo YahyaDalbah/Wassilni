9fc807dceed57e11fb30ff0918aaea9ed2eaa4d3
YahyaDalbah <yahya1dalbah@gmail.com>
2025-05-25 15:06:30 +0300

    when ride becomes in_progress, go to to_destination

---
+++ lib/pages/to_destination.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import "map.dart";
+class ToDestination extends StatefulWidget {
+  const ToDestination({super.key});
+
+  @override
+  State<ToDestination> createState() => _ToDestinationState();
+}
+
+class _ToDestinationState extends State<ToDestination> {
+  @override
+  Widget build(BuildContext context) {
+    return const Map();
+  }
+}
